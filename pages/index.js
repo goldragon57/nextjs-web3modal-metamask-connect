34788f1b9a8fd0ff64f78974dc481857cf5653d8
goldragon57 <125270058+goldragon57@users.noreply.github.com>
2023-02-19 00:35:35 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,12 @@ import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { abi } from "../constants/abi";
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+import Token from './artifacts/contracts/Token.sol/Token.json'
+
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 let web3Modal;
 
@@ -29,13 +35,69 @@ export default function Home() {
   const [isConnected, setIsConnected] = useState(false);
   const [hasMetamask, setHasMetamask] = useState(false);
   const [signer, setSigner] = useState(undefined);
+  const [greeting, setGreetingValue] = useState()
+  const [userAccount, setUserAccount] = useState()
+  const [amount, setAmount] = useState()
+  const [address, setAddress] = useState("")
 
   useEffect(() => {
     if (typeof window.ethereum !== "undefined") {
       setHasMetamask(true);
     }
   });
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
 
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      console.log({ provider })
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }    
+  }
+
+  async function getBalance() {
+    if (typeof window.ethereum !== 'undefined') {
+      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
+      const balance = await contract.balanceOf(account);
+      console.log("Balance: ", balance.toString());
+    }
+  }
+
+  async function setGreeting() {
+    if (!greeting) return
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log({ provider })
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+
+  async function sendCoins() {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
+      const transation = await contract.transfer(userAccount, amount);
+      await transation.wait();
+      console.log(`${amount} Coins successfully sent to ${userAccount}`);
+    }
+  }
   async function connect() {
     if (typeof window.ethereum !== "undefined") {
       try {
@@ -66,10 +128,11 @@ export default function Home() {
   }
 
   return (
-    <div>
+    <div className="App">
+      <div className="body">
       {hasMetamask ? (
         isConnected ? (
-          "Connected! "
+          <a className ="connect">Wallet Connected!</a>
         ) : (
           <button onClick={() => connect()}>Connect</button>
         )
@@ -78,6 +141,18 @@ export default function Home() {
       )}
 
       {isConnected ? <button onClick={() => execute()}>Execute</button> : ""}
+      <div className=" fectchGreeting"> <button onClick={fetchGreeting}>Fetch Greeting</button></div>
+        <div>
+          <button onClick={setGreeting}>Set Greeting</button>
+          <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+        </div>
+        <br />
+        <button onClick={getBalance}>Get Balance</button>
+        <br />
+        <button onClick={sendCoins}>Send Coins</button>
+        <input onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
+        <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
+        </div>
     </div>
   );
 }
